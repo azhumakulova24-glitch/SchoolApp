da1b2d5b57a317b870f76c0dccad41e1bd650eed
asylaizh <asiko@DESKTOP-1KRJD6Q>
2026-05-28 22:12:45 +0500

    L15: data binding on MainPage

--- SchoolApp/MainPage.xaml.cs
+++ SchoolApp/MainPage.xaml.cs
@@ -1,17 +1,24 @@
-﻿namespace SchoolApp;
+﻿using SchoolApp.ViewModels;
+namespace SchoolApp;
 
 public partial class MainPage : ContentPage
 {
     private int _count = 0;
+
     public MainPage()
     {
         InitializeComponent();
+
+       
+        BindingContext = new Person();
     }
+    
     private void OnTapClicked(object sender, EventArgs e)
     {
         _count++;
         StatusLabel.Text = $"Button tapped {_count} time(s).";
     }
+
     private async void OnOpenStudentsClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync(nameof(StudentsPage));
